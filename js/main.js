57f37ab1c829327c314c8e315352df9179e9bc09
Max Rebryk <rebryk2003@gmail.com>
2024-02-11 18:12:15 +0300

    mobile-menu-buttons-js

--- js/main.js
+++ js/main.js
@@ -1,16 +1,31 @@
 "use strict";
 
-const orderButton = document.querySelector(".button-order");
-
+// Функция открытия/закрытия бэкдропа
 
 const toggleBackdrop = () => {
     const backdrop = document.querySelector(".backdrop")
     backdrop.classList.toggle("is-open");
 }
 
+// Кнопки заказать услуги и закрыть форму
+
+const orderButton = document.querySelector(".button-order");
 orderButton.addEventListener("click",toggleBackdrop);
 
 const closeBackdropButton = document.querySelector(".close-btn");
 
 closeBackdropButton.addEventListener("click",toggleBackdrop);
 
+// Функция открытия/закрытия меню
+
+const toggleMenu = () => {
+    const menu = document.querySelector(".mobile-menu-backdrop")
+    menu.classList.toggle("is-open");
+}
+// Кнопки открытия/закрытия мобильного меню
+
+const openMobileMenuButton = document.querySelector(".menu-btn");
+openMobileMenuButton.addEventListener("click",toggleMenu)
+
+const closeMobileMenuButton = document.querySelector(".close-menu-btn");
+closeMobileMenuButton.addEventListener("click",toggleMenu);
